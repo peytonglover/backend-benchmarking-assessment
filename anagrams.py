6d1a7c8ba89b20dfe80a928e1f1424a0b536efc9
peyton glover <peyton.vx@protonmail.com>
2020-06-30 15:21:31 -0400

    donezo

--- anagrams.py
+++ anagrams.py
@@ -27,11 +27,14 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        if alphabetize(word) in anagrams:
+            anagrams[alphabetize(word)] += [word]
+        else:
+            anagrams[alphabetize(word)] = [word]
+
+    print(anagrams)
     return anagrams
 
 
